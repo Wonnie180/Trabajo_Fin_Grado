eb6a6a03d6365a5a51db31e4597ac23687bdb07b
Wonnie180 <llamas.120781@e.unavarra.es>
2023-01-09 22:27:20 +0100

    [Memoria] 09/01/2023 22:20
    [Código Python] Bugfix ICommand_Go_To_Position_2D

--- Codigo/PC/Python/Commands/Command_Go_To_Position/Command_Go_To_Position_2D/ICommand_Go_To_Position_2D.py
+++ Codigo/PC/Python/Commands/Command_Go_To_Position/Command_Go_To_Position_2D/ICommand_Go_To_Position_2D.py
@@ -6,7 +6,7 @@ sys.path.append(
     os.path.join(os.path.dirname(__file__), ".." + os.path.sep + ".." + os.path.sep+ ".." + os.path.sep)
 )
 
-from Commands.Command_Go_To_Position import ICommand_Go_To_Position
+from Commands.Command_Go_To_Position.ICommand_Go_To_Position import ICommand_Go_To_Position
 from Tropas.ITropa import ITropa
 from Positions.IPosition import IPosition
 from Arucos.IAruco import IAruco
@@ -21,17 +21,21 @@ class ICommand_Go_To_Position_2D(ICommand_Go_To_Position, metaclass=ABCMeta):
         self.tropa = tropa
         self.objective_position = objective_position
         self.distance_threshold = distance_threshold
-        super().__init__()
+        super().__init__(aruco, tropa, objective_position, distance_threshold)
 
+    @abstractmethod
     def Is_Facing_Objective(self, angle):
         pass
 
+    @abstractmethod
     def Is_Back_Facing_Objective(self, angle):
        pass
 
+    @abstractmethod
     def Has_To_Rotate_Right(self, angle):
         pass
 
+    @abstractmethod
     def Movement_Action(self, angle):
         pass
 
